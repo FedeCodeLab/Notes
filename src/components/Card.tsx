1e5893715bedb866dc0cc2cfd1b8efe58cd8eded
fedecodelab <federicoguzman.css@gmail.com>
2024-09-21 18:09:45 -0300

    card finalizada

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,22 @@
 import EditIcon from "@/icons/EditIcon";
 import TrashIcon from "@/icons/TrashIcon";
+import TrashIcon2 from "@/icons/TrashIcon2";
 
 export default function Card() {
   return (
-    <article className="bg-white border rounded-lg p-6 gap-2 flex flex-col">
+    <article className="bg-white hover:bg-slate-100 border rounded-lg p-6 gap-2 flex flex-col cursor-pointer">
       <div className="flex gap-2">
         <h3 className="font-semibold">
           Hoy tuve un sueño acerca de una vida pasada
         </h3>
 
-        <div className="">
-          <EditIcon color={"#fff"} width={20} height={20} />
-        </div>
+        <button className="bg-red-500 hover:bg-red-600 rounded-full w-8 h-8 p-2 flex items-center justify-center text-white">
+          <TrashIcon2 color={"#fff"} width={16} height={16} />
+        </button>
 
-        {/* <button className="bg-emerald-700 hover:bg-emerald-800 rounded-full  flex items-center justify-center text-white">
-          <TrashIcon color={"#fff"} width={10} height={10} />
-        </button> */}
+        <button className="bg-emerald-700 hover:bg-emerald-800 rounded-full w-8 h-8 p-2 flex items-center justify-center text-white">
+          <EditIcon color={"#fff"} width={16} height={16} />
+        </button>
       </div>
       <p className="text-sm text-zinc-500">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod

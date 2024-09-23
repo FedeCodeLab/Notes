4ce36173b693045b7a979a001bd99d0e663933e4
fedecodelab <federicoguzman.css@gmail.com>
2024-09-23 15:57:09 -0300

    instalando zustand y quill

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,4 +1,24 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import "react-quill/dist/quill.snow.css";
+import { useState } from "react";
+
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+
 export default function Create() {
+  const [value, setValue] = useState("");
+
+  const modules = {
+    toolbar: [
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
   return (
     <section className="flex flex-col gap-3">
       <input
@@ -6,12 +26,14 @@ export default function Create() {
         placeholder="Título de la página"
         className="w-full p-3 border text-lg text-neutral-600"
       />
-      <textarea
-        name=""
-        id=""
-        placeholder="Escribe algo"
-        className="w-full min-h-80 max-h-96 p-3 border"
-      ></textarea>
+
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={setValue}
+        modules={modules}
+        className="h-full bg-white"
+      />
     </section>
   );
 }
